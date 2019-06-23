5add43a1302acb1c7fb7107301d59702a1be0c16
Bruna Nobre Almeida <brunanobrealmeida@gmail.com>
2019-06-22 22:41:25 -0300

    Content traslation

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card register-card">
-                <div class="card-header">{{ __('Register') }}</div>
+                <div class="card-header">Cadastre-se</div>
 
                 <div class="card-body">
                     <form class="register-form" method="POST" action="{{ route('register') }}">
@@ -15,7 +15,7 @@
 
                             <div class="col-md-6">
                                 <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
-                                <label id="name-label" for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
+                                <label id="name-label" for="name" class="col-md-4 col-form-label text-md-right">Qual é o seu nome?</label>
 
                                 @error('name')
                                     <span class="invalid-feedback" role="alert">
@@ -29,7 +29,7 @@
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-                                <label id="email-label" for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                                <label id="email-label" for="email" class="col-md-4 col-form-label text-md-right">E-mail</label>
 
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
@@ -43,7 +43,7 @@
 
                             <div class="col-md-6">
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-                                <label id="password-label" for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+                                <label id="password-label" for="password" class="col-md-4 col-form-label text-md-right">Senha</label>
 
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
@@ -57,7 +57,7 @@
 
                             <div class="col-md-6">
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                                <label id="password-confirm-label" for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
+                                <label id="password-confirm-label" for="password-confirm" class="col-md-4 col-form-label text-md-right">Confirme sua senha</label>
 
                             </div>
                         </div>
@@ -65,7 +65,7 @@
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="above-btn btn btn-primary">
-                                    {{ __('Register') }}
+                                    Confirmar
                                 </button>
                             </div>
                         </div>
