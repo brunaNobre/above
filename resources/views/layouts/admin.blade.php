c1e723ef644c362ae8d9f0ad0d3728dad043e44a
Bruna Nobre Almeida <brunanobrealmeida@gmail.com>
2019-06-27 18:36:45 -0300

    admin layout page

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -27,9 +27,7 @@
 <!-- Sidebar/menu -->
 <nav class="w3-sidebar w3-collapse w3-white" style="z-index:3;width:300px;" id="mySidebar"><br>
     <div class="w3-container w3-row">
-        <div class="w3-col s4">
-            <img src="/fotos/users/nina-simone.jpg" class="w3-circle w3-margin-right" style="width:46px">
-        </div>
+     
         <div class="w3-col s8 w3-bar">
             @php
                $user_name = explode(" ", Auth::user()->name);
