3e3021e5f12408cc420e2724bd72dc0b623ba4b6
Bruna Nobre Almeida <brunanobrealmeida@gmail.com>
2019-06-27 15:19:27 -0300

    Mood Model config

--- app/Mood.php
+++ app/Mood.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Mood extends Model
 {
-    //
+    protected $fillable = ['user_id', 'status', 'date'];
+    
+    public function user() {
+        return $this->belongsTo('App\User');
+    }
+   
+    
 }
