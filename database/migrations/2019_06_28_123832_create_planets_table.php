689dde23f7a25a8c0224737cb7fe60561aaf67b1
Bruna Nobre Almeida <brunanobrealmeida@gmail.com>
2019-06-28 09:53:07 -0300

    planets migration update

--- database/migrations/2019_06_28_123832_create_planets_table.php
+++ database/migrations/2019_06_28_123832_create_planets_table.php
@@ -14,7 +14,10 @@ class CreatePlanetsTable extends Migration
     public function up()
     {
         Schema::create('planets', function (Blueprint $table) {
-            //
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('description');
+            $table->timestamps();
         });
     }
 
