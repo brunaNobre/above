447d4f5764bdf9bf6f449659edc6fbe6d93c033c
Bruna Nobre Almeida <brunanobrealmeida@gmail.com>
2019-06-30 03:18:08 -0300

    layout update

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -23,7 +23,7 @@
 <!-- Top container -->
 <div class="w3-bar w3-top w3-black w3-large" style="z-index:4">
     <button class="w3-bar-item w3-button w3-hide-large w3-hover-none w3-hover-text-light-grey" onclick="w3_open();"><i class="fa fa-bars"></i>  Menu</button>
-    <a href="/admin" class="w3-bar-item w3-right">Above</a>
+    <a href="/admin" class="admin-logo w3-bar-item w3-right">Above</a>
 </div>
 
 <!-- Sidebar/menu -->
