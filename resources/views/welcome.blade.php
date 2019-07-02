fcc1c1a1b896f58fa94534c299625f8fa156d05c
Bruna Nobre Almeida <brunanobrealmeida@gmail.com>
2019-07-02 12:22:13 -0300

    Layout update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>Above - Sua vida virginiane.</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed:light,regular,bold|Arvo:regular,bold|Roboto:thin,light,regular,medium,bold,black" rel="stylesheet">
