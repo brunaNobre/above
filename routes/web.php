ca7582c84ea6446ce415e3c5a0e1f9d7a29f2d6a
Bruna Nobre Almeida <brunanobrealmeida@gmail.com>
2019-08-05 13:47:55 -0300

    web routes update

--- routes/web.php
+++ routes/web.php
@@ -29,7 +29,7 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::get('admin', 'AdminController@index')->name('admin.dashboard');
+Route::get('/admin', 'AdminController@index')->name('admin.dashboard');
 
 
 
