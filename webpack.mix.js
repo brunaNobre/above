4d666256847fe369585f795ee93c5b58d4027ab2
Bruna Nobre Almeida <brunanobrealmeida@gmail.com>
2019-09-20 19:42:06 -0300

    react scaffolding

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,8 +11,5 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css')
-    .sass('resources/sass/admin/admin.scss', 'public/css/admin');
-
-    
+mix.react('resources/js/app.js', 'public/js')
+   .sass('resources/sass/app.scss', 'public/css');
