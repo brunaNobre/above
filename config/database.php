9645ae7596e3e932ea0ae69bc47bd145e6c623dc
Bruna Nobre Almeida <brunanobrealmeida@gmail.com>
2019-09-21 14:42:11 -0300

    database configuration

--- config/database.php
+++ config/database.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'system'),
+    'default' => env('DB_CONNECTION', 'mysql'),
 
     /*
     |--------------------------------------------------------------------------
