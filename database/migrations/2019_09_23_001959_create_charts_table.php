60f770be0e6e878a7e2c54a16ac532c53be75fab
Bruna Nobre Almeida <brunanobrealmeida@gmail.com>
2019-09-22 21:32:47 -0300

    database update: add migrations

---
+++ database/migrations/2019_09_23_001959_create_charts_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateChartsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('charts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('userId');
+            $table->string('sun');
+            $table->string('rising_sign');
+            $table->string('moon');
+            $table->string('mercury');
+            $table->string('venus');
+            $table->string('mars');
+            $table->string('jupiter');
+            $table->string('saturn');
+            $table->string('uranus');
+            $table->string('neptune');
+            $table->string('pluto');
+            $table->string('mc');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('charts');
+    }
+}
