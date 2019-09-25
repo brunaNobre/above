eb2f3515a94b5f97fbbb1a16205d42465c44e3ef
Bruna Nobre Almeida <brunanobrealmeida@gmail.com>
2019-09-25 16:46:27 -0300

    moons list view layout refactor

--- resources/views/admin/moons_list.blade.php
+++ resources/views/admin/moons_list.blade.php
@@ -2,54 +2,53 @@
 
 @section('content')
 
-<div class='col-sm-8'>
-    <h2 class="sec-title">Fases da Lua </h2> 
-</div>
-<div class='col-sm-4'>
-    @if (session('status'))
-        <div class="alert alert-success">
-            {{ session('status') }}
-        </div>
-    @endif
-</div>
+<header class="abv-grid">
+    <h2>Fases da Lua</h2>
 
-<div class="w3-container">
+    <div class="abv-btn-wrapper">
+    <a href="{{route('moons.create')}}" role="button" class="abv-admin-btn">+</a>
 
-<table class="w3-table w3-striped w3-bordered w3-border w3-hoverable w3-white w3-ul w3-card-4">
+    </div>
+
+</header>
+
+
+<div class="abv-card">
+    <table class="table">
     <thead>
-      <tr>
-        <th>Fase</th>
-        <th>Descriçao</th> 
-      </tr>
+        <tr>
+            <th scope="col">#</th>
+            <th scope="col">Nome</th>
+            <th scope="col">Descrição</th>
+            <th scope="col"></th>
+        </tr>
     </thead>
     <tbody>
-@foreach($moons as $moon)
-<tr>
-    <td>{{$moon->phase}}</td>
-    <td>{{$moon->description}}</td>
-    
-    <td>
-       <a href="{{route('moons.edit', $moon->id)}}" 
-          class="btn btn-warning" 
-          role="button">Alterar</a> &nbsp;&nbsp;
+        @foreach($moons as $moon)
+        <tr>
+            <th scope="row">{{$moon->id}}</th>
+            <td>{{$moon->name}}</td>
+            <td>{{$moon->description}}</td>
+            <td>
+
+
+                    <a href="{{route('moons.edit', $moon->id)}}" 
+          class="abv-edit-button" 
+          role="button"><i class="abv-edit-icon fas fa-pen"></i></a>
        <form style="display: inline-block"
              method="post"
              action="{{route('moons.destroy', $moon->id)}}"
              onsubmit="return confirm('Confirma Exclusão?')">
              {{method_field('delete')}}
              {{csrf_field()}}
-             <button type="submit"
-                     class="btn btn-danger"> Excluir </button>
+             <button type="submit" class="abv-detele-button"> <i class="abv-delete-icon fas fa-trash-alt"></i> </button>
        </form> 
-    </td>
-</tr>
-@endforeach
-    <tr> <td><a href="{{route('moons.create')}}" class="above-admin-btn btn btn-info"
-                role="button">Novo</a></td></tr>
+
+            </td>
+        </tr>
+        @endforeach   
     </tbody>
-  </table>    
+    </table>
+        
 </div>
-
-
-
 @endsection
