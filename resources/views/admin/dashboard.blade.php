43e719a336e44218b502b892de62742d61bd0a3c
Bruna Nobre Almeida <brunanobrealmeida@gmail.com>
2019-09-25 16:37:10 -0300

    planets list view layout refactor

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -2,50 +2,7 @@
 
 @section('content')
 
-<header class="abv-grid">
-    <h2>Planetas</h2>
 
-    <div class="abv-btn-wrapper">
-    <a href="{{route('planets.create')}}" role="button" class="abv-admin-btn">+</a>
-
-    </div>
-
-</header>
-
-
-<div class="abv-card">
-    <table class="table">
-    <thead>
-        <tr>
-            <th scope="col">#</th>
-            <th scope="col">First</th>
-            <th scope="col">Last</th>
-            <th scope="col">Handle</th>
-        </tr>
-    </thead>
-    <tbody>
-        <tr>
-            <th scope="row">1</th>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
-        </tr>
-        <tr>
-            <th scope="row">2</th>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>@fat</td>
-        </tr>
-        <tr>
-            <th scope="row">3</th>
-            <td>Larry</td>
-            <td>the Bird</td>
-            <td>@twitter</td>
-        </tr>
-    </tbody>
-    </table>
-        
-</div>
 
 
 @endsection
--- resources/views/admin/planets_list.blade.php
+++ resources/views/admin/planets_list.blade.php
@@ -2,52 +2,53 @@
 
 @section('content')
 
-    <h2 class="sec-title"> Planetas </h2>
-<div class='col-sm-4'>
-    @if (session('status'))
-        <div class="alert alert-success">
-            {{ session('status') }}
-        </div>
-    @endif
-</div>
+<header class="abv-grid">
+    <h2>Planetas</h2>
+
+    <div class="abv-btn-wrapper">
+    <a href="{{route('planets.create')}}" role="button" class="abv-admin-btn">+</a>
+
+    </div>
+
+</header>
+
 
 <div class="abv-card">
-
-<table class="w3-table w3-striped w3-bordered w3-border w3-hoverable w3-white w3-ul w3-card-4">
+    <table class="table">
     <thead>
-      <tr>
-        <th>Nome</th>
-        <th>Descriçao</th> 
-      </tr>
+        <tr>
+            <th scope="col">#</th>
+            <th scope="col">Nome</th>
+            <th scope="col">Descrição</th>
+            <th scope="col"></th>
+        </tr>
     </thead>
     <tbody>
-@foreach($planets as $planet)
-<tr>
-    <td>{{$planet->name}}</td>
-    <td>{{$planet->description}}</td>
-    
-    <td>
-       <a href="{{route('planets.edit', $planet->id)}}" 
-          class="btn btn-warning" 
-          role="button">Alterar</a> &nbsp;&nbsp;
+        @foreach($planets as $planet)
+        <tr>
+            <th scope="row">{{$planet->id}}</th>
+            <td>{{$planet->name}}</td>
+            <td>{{$planet->description}}</td>
+            <td>
+
+
+                    <a href="{{route('planets.edit', $planet->id)}}" 
+          class="abv-edit-button" 
+          role="button"><i class="abv-edit-icon fas fa-pen"></i></a>
        <form style="display: inline-block"
              method="post"
              action="{{route('planets.destroy', $planet->id)}}"
              onsubmit="return confirm('Confirma Exclusão?')">
              {{method_field('delete')}}
              {{csrf_field()}}
-             <button type="submit"
-                     class="btn btn-danger"> Excluir </button>
+             <button type="submit" class="abv-detele-button"> <i class="abv-delete-icon fas fa-trash-alt"></i> </button>
        </form> 
-    </td>
-</tr>
-@endforeach
-    <tr> <td><a href="{{route('planets.create')}}" class="above-admin-btn"
-                role="button">Novo</a></td></tr>
+
+            </td>
+        </tr>
+        @endforeach   
     </tbody>
-  </table>    
+    </table>
+        
 </div>
-
-
-
 @endsection
