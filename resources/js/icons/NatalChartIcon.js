2f68adbd54d0affdaf15e612aa1dffdd30b35e64
Bruna Nobre Almeida <brunanobrealmeida@gmail.com>
2019-10-18 15:42:33 -0300

    create nata chart icon

---
+++ resources/js/icons/NatalChartIcon.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function NatalChartIcon () {
+    return (
+        <img src="/images/icons/icon_natalchart.png" className="natalchart-icon"></img>
+    )
+}
+
+export default NatalChartIcon
