2739cc5e38d7f918dabc88108125dfba9a1268e9
Bruna Nobre Almeida <brunanobrealmeida@gmail.com>
2019-10-29 15:00:20 -0300

    fix layout

--- resources/js/components/layouts/AboveMainContent.js
+++ resources/js/components/layouts/AboveMainContent.js
@@ -3,6 +3,7 @@ import Panel from '../Panel'
 import MoonHeader from '../MoonHeader'
 import formatDate from '../../utils/formatDate'
 import getPhase from '../../utils/getPhase'
+import NewTaskDialog from './NewTaskDialog'
 
 
 
@@ -10,13 +11,50 @@ class AboveMainContent extends Component {
     constructor() {
         super()
         this.state = {
-            phase: ""
+            phase: "",
+            newTask: {
+                user_id: 0,
+                title: "",
+                due_to: formatDate(new Date().toLocaleDateString())
+            }
         }
+        this.handleAdd = this.handleAdd.bind(this)    
+        this.sendInputValue = this.sendInputValue.bind(this)
     }
 
     componentDidMount() {
         var today_date = new Date(formatDate(new Date().toLocaleDateString()))
-        this.setState({phase: getPhase(today_date)})  
+        this.setState({phase: getPhase(today_date)});
+
+        axios.get(`/api/user`)
+        .then(res => {
+          this.setState({
+              newTask: {
+                  user_id: res.data.id,
+                  title: "",
+                  due_to: formatDate(new Date().toLocaleDateString())
+              },
+          })
+        })
+    }
+
+    handleAdd (newTask) {
+        if(newTask.title) {
+            axios.post('/api/tasks', newTask);
+
+            this.setState({
+                newTask: {
+                    user_id: this.state.newTask.user_id,
+                    title: "",
+                    due_to: formatDate(new Date().toLocaleDateString())
+                }
+            }) 
+       }
+    };
+
+
+    sendInputValue (key, newValue) {
+        this.setState((state) => state.newTask[key] = newValue);
     }
 
     render() {
@@ -24,6 +62,11 @@ class AboveMainContent extends Component {
             <div>
                 <Panel />
                 <MoonHeader phase={this.state.phase}/>
+                <NewTaskDialog
+                newTask={this.state.newTask} 
+                sendInputValue={this.sendInputValue}
+                handleAdd={this.handleAdd}
+                />
             </div>
         )
     }
