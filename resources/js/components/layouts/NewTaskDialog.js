3c545569c4023854ad3d8a3e62291fe43b1d3957
Bruna Nobre Almeida <brunanobrealmeida@gmail.com>
2019-10-29 14:50:55 -0300

    add plus button to add new task from all the views

---
+++ resources/js/components/layouts/NewTaskDialog.js
@@ -0,0 +1,87 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItem from '@material-ui/core/ListItem';
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import CloseIcon from '@material-ui/icons/Close';
+import Slide from '@material-ui/core/Slide';
+import NewTaskFields from '../Tasks/NewTaskFields'
+import sunSign from '../../utils/sunSign'
+import AddIcon from '@material-ui/icons/Add';
+
+
+
+
+const useStyles = makeStyles(theme => ({
+  appBar: {
+    position: 'relative',
+  },
+  title: {
+    marginLeft: theme.spacing(2),
+    flex: 1,
+  },
+}));
+
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
+
+export default function NewTaskForm(props) {
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
+ 
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleCloseAndSaveTask = () => {
+    setOpen(false);
+    props.handleAdd(props.newTask)
+  };
+
+
+
+  return (
+    <div>
+      <Button className="plus-button" variant="outlined" onClick={handleClickOpen}>
+      <AddIcon />
+      </Button>
+
+      <Dialog fullScreen open={open} onClose={handleClose} TransitionComponent={Transition}>
+        <AppBar className={classes.appBar + " newtask-header"}>
+          <Toolbar>
+            <IconButton edge="start" color="inherit" onClick={handleClose} aria-label="close">
+              <CloseIcon />
+            </IconButton>
+            <Typography variant="h6" className={classes.title}>
+              Nova tarefa
+            </Typography>
+            <Button color="inherit" onClick={handleCloseAndSaveTask}>
+              salvar
+            </Button>
+          </Toolbar>
+        </AppBar>
+        
+        <NewTaskFields 
+        newTask={props.newTask}
+        sendInputValue={props.sendInputValue}
+         />
+
+         <p className="new-task-form-sunsign">Na data acima o <span>Sol</span> está em <span>{sunSign(props.newTask.due_to)}</span></p>
+
+      </Dialog>
+    </div>
+  );
+}
--- resources/js/views/Planets.js
+++ resources/js/views/Planets.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react'
 import PlanetCard from '../components/Planets/PlanetCard'
+import NewTaskDialog from '../components/layouts/NewTaskDialog'
+import formatDate from '../utils/formatDate'
 
 
 class Planets extends Component {
     constructor() {
         super()
         this.state = {
-            planets: []
+            planets: [],
+            newTask: {
+                user_id: 0,
+                title: "",
+                due_to: formatDate(new Date().toLocaleDateString())
+            }
         }
+
+        this.handleAdd = this.handleAdd.bind(this)    
+        this.sendInputValue = this.sendInputValue.bind(this)
     }
 
     componentDidMount() {
@@ -15,7 +25,39 @@ class Planets extends Component {
           .then(res => {
              this.setState({planets: res.data})
           })
+
+          axios.get(`/api/user`)
+          .then(res => {
+            this.setState({
+                newTask: {
+                    user_id: res.data.id,
+                    title: "",
+                    due_to: formatDate(new Date().toLocaleDateString())
+                },
+            })
+          })
     }
+
+    handleAdd (newTask) {
+        if(newTask.title) {
+            axios.post('/api/tasks', newTask);
+
+            this.setState({
+                newTask: {
+                    user_id: this.state.newTask.user_id,
+                    title: "",
+                    due_to: formatDate(new Date().toLocaleDateString())
+                }
+            }) 
+       }
+    };
+
+
+    sendInputValue (key, newValue) {
+        this.setState((state) => state.newTask[key] = newValue);
+    }
+
+    
     
 
     render() {
@@ -26,6 +68,11 @@ class Planets extends Component {
         return (
             <div className="planets-view">
               {planetCards}
+              <NewTaskDialog
+                newTask={this.state.newTask} 
+                sendInputValue={this.sendInputValue}
+                handleAdd={this.handleAdd}
+                />
             </div>
         )
     }
