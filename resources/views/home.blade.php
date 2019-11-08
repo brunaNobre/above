5c6ff44db009d0e315270f231f29c59dbb0e98f5
Bruna Nobre Almeida <brunanobrealmeida@gmail.com>
2019-11-08 01:06:00 -0300

    secure asset

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,6 +7,6 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 </div>
 
-<script src="{{ asset('js/app.js') }}"></script>
+<script src="{{ secure_asset('js/app.js') }}"></script>
 
 @endsection
