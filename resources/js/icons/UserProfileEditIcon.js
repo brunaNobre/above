3e72600e2a2d91e1475ad5344b4c619f37437101
Bruna Nobre Almeida <brunanobrealmeida@gmail.com>
2020-01-29 17:57:02 -0300

    remove placeholder profile images

--- resources/js/icons/UserProfileEditIcon.js
+++ resources/js/icons/UserProfileEditIcon.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function UserProfileEditIcon () {
     return (
-        <div className="userProfileEditIcon-wrapper">
+        <div className="userProfileEditIcon-wrapper" alt="Editar foto do perfil" title="Editar foto do perfil">
             <div className="userProfileEditIcon-background">
             </div>
         </div>
